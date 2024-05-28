3c8a017701b324f22fd4e5b78224b7eb8a898afe
Nadia Liubynska <n.liubynska@gmail.com>
2024-05-28 16:22:48 +0300

    Added a new database test

--- tests/database/test_database.py
+++ tests/database/test_database.py
@@ -3,7 +3,15 @@ from modules.common.database import Database
 
 
 @pytest.mark.database
-@pytest.mark.wip
 def test_database_connection():
     db = Database()
     db.test_connection()
+
+
+@pytest.mark.database
+@pytest.mark.wip
+def test_check_all_users():
+    db = Database()
+    users = db.get_all_users()
+
+    print(users)
